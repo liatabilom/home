d81466b90e8c859cbab389a5d7a331d474b78098
Lia <lia.tabilo@gmail.com>
2019-09-03 18:59:06 +0100

    Add links to pages component

--- src/Pages.js
+++ src/Pages.js
@@ -8,17 +8,21 @@ class Pages extends Component {
         <nav aria-label="footer">
           <ul className="bottom-pages">
             <li className="btn btn-outline-dark rounded-1">
-              <a className="page-button" href="#">
-                Page 1
-              </a>
+              <a className="page-button">Page 1</a>
             </li>
             <li className="btn btn-outline-dark rounded-1 ">
-              <a className="page-button" href="/SecondMain">
+              <a
+                className="page-button"
+                href="https://compassionate-liskov-30453f.netlify.com/"
+              >
                 Page 2
               </a>
             </li>
             <li className="btn btn-outline-dark d-table-cell rounded-1 ">
-              <a className="page-button" href="#">
+              <a
+                className="page-button"
+                href="https://fervent-wing-877df5.netlify.com/"
+              >
                 Page 3
               </a>
             </li>
