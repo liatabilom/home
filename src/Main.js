9c8f1457c00f03f291516df0c3262b11a3980425
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2020-09-21 20:20:24 +0100

    general padding

--- src/Main.js
+++ src/Main.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react';
-import Search from './Search';
-import AllCards from './AllCards';
-import Pages from './Pages';
-import './Main.css';
+import React, { Component } from "react";
+import Search from "./Search";
+import AllCards from "./AllCards";
+import Pages from "./Pages";
+import "./Main.css";
 
 class Main extends Component {
-	render() {
-		return (
-			<div>
-				<div className="header">
-					<header>
-						Na'dia
-						<br />
-						Correia Gonc,alves
-					</header>
-				</div>
-				<Search />
-				<div className="row">
-					<div className="side">
-						<h2>
-							<AllCards />
-						</h2>
-					</div>
-				</div>
-				<div className="footer">
-					<h2>
-						<Pages />
-					</h2>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="mainHeader">
+        <div className="header">
+          <header>
+            Na'dia
+            <br />
+            Correia Gonc,alves
+          </header>
+        </div>
+        <Search />
+        <div className="row">
+          <div className="side">
+            <h2>
+              <AllCards />
+            </h2>
+          </div>
+        </div>
+        <div className="footer">
+          <h2>
+            <Pages />
+          </h2>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Main;
