37eb2fc9d87aad088b7d90f9e8e9aa174fafd831
nadiacoralves <55755193+nadiacoralves@users.noreply.github.com>
2020-09-21 20:46:28 +0100

    email and buttons adjustment

--- src/Search.js
+++ src/Search.js
@@ -1,54 +1,66 @@
-import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import './Search.css';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import "./Search.css";
 
 class Search extends Component {
-	state = '';
-	preventReload = event => {
-		event.preventDefault();
-		this.setState({});
-	};
+  state = "";
+  preventReload = (event) => {
+    event.preventDefault();
+    this.setState({});
+  };
 
-	render() {
-		return (
-			<div className="searchBar">
-				<h1>
-					<div className="clearfix">
-						<form onSubmit={this.preventReload}>
-							<input className="emailInput float-left" type="text" placeholder="Email" />
-							<a
-								href="mailto:nadiacoralves@gmail.com?Subject=Hi%20Nadia"
-								target="_top"
-								className="submitEmail"
-								style={{ color: 'black' }}
-							>
-								<FontAwesomeIcon icon={faChevronRight} size="1x" />
-							</a>
-						</form>
-						<span className="infoIcon float-left ml-3">
-							<a href="https://info.correiagoncalves.com/" style={{ color: 'black' }}>
-								<FontAwesomeIcon icon={faInfoCircle} size="1x" />
-							</a>
-						</span>
-						<span className="avocadoIcon float-left ml-3" role="img" aria-label="Avocado">
-							<a
-								href="https://nadiacoralves.tumblr.com/"
-								target="_blank"
-								rel="noopener noreferrer"
-								style={{ textDecoration: 'none' }}
-							>
-								<span role="img" aria-label="avocado">
-									🥑
-								</span>
-							</a>
-						</span>
-					</div>
-				</h1>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="searchBar">
+        <h1>
+          <div className="clearfix">
+            <form onSubmit={this.preventReload}>
+              <a
+                href="mailto:nadiacoralves@gmail.com?Subject=Hi%20Nadia"
+                target="_top"
+                className="emailInput"
+                style={{ color: "black" }}
+              >
+                <input
+                  className="emailInput float-left"
+                  type="text"
+                  placeholder="Email"
+                />
+
+                <FontAwesomeIcon icon={faChevronRight} size="1x" />
+              </a>
+            </form>
+            <span className="infoIcon float-left ml-3">
+              <a
+                href="https://info.correiagoncalves.com/"
+                style={{ color: "black" }}
+              >
+                <FontAwesomeIcon icon={faInfoCircle} size="1x" />
+              </a>
+            </span>
+            <span
+              className="avocadoIcon float-left ml-3"
+              role="img"
+              aria-label="Avocado"
+            >
+              <a
+                href="https://nadiacoralves.tumblr.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ textDecoration: "none" }}
+              >
+                <span role="img" aria-label="avocado">
+                  🥑
+                </span>
+              </a>
+            </span>
+          </div>
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default Search;
